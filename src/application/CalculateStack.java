3d14259fa34214cac10e850e2057970e27efb0ee
Mirish <mirishlaen@hotmail.com>
2017-01-18 11:10:00 +0200

    Start fill up the stack

---
+++ src/application/CalculateStack.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Stack;
+
+public class CalculateStack {
+	Stack<StackItem> opStack = new Stack<StackItem>();
+	logicStrips logic = new logicStrips();
+	
+	CalculateStack(){
+		for(Furniture fur: logic.furnitures){
+			
+		}
+	}
+}
---
+++ src/application/StackItem.java
@@ -0,0 +1,14 @@
+package application;
+
+public class StackItem {
+	Furniture fur; 
+	StackItem(Furniture fur){
+		this.fur = fur;
+	}
+	
+	public Furniture getFur(){
+		return fur;
+	}
+}
+
+
